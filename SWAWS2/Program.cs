ad51c442ce94cc5bbb955bb56dd090820e746b41
EmilWallett <nhlpool27@gmail.com>
2022-08-29 10:36:19 +0200

    Initial commit

---
+++ SWAWS2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Är det fint väder idag?");
+string Väder = Console.ReadLine();
+if(Väder == "j") 
+Console.Write("vi går på picknick!");
+else if(Väder == "J") 
+Console.Write("vi går på picknick!");
+else if(Väder == "n")
+Console.Write("vi stannar inne och läser en bok");
+else if(Väder == "N")
+Console.Write("vi stannar inne och läser en bok");
+else
+Console.Write("Jag förstår inte");
+
+
+
+
+
+ 
+
